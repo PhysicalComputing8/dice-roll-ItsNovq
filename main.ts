694651811a47d061bbbddb621bbc2a2014fe6f48
Trevor Shaw <shawt@genlrn.com>
2020-09-22 08:17:51 -0400

    Teacher Feedback.

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+// Great Job! You moved the code into the onShake event trigger. This way it will be executed each time the micro:bit is shaken.
 input.onGesture(Gesture.Shake, function () {
+    // Awesome! You correctly set the limits of the dice roll to between 1 and 6.
     roll = randint(1, 6)
     basic.showNumber(roll)
 })
