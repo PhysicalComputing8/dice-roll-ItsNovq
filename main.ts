31bee91dcbaccafeabdced9d3cba5a4ffd8be1a0
Trevor Shaw <shawt@genlrn.com>
2020-09-23 11:54:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,5 +4,11 @@ input.onGesture(Gesture.Shake, function () {
     roll = randint(1, 6)
     basic.showNumber(roll)
 })
+// Alan,
+// Not a bad start, but I was hoping that by the end of class you would have at least some of the advanced parts of the assignment done.
+// 
+// Please let me know if you are having trouble, so I can help you stay caught up.
+// 
+// Mr. Shaw
 let roll = 0
 basic.showString("Shake to roll the dice")
